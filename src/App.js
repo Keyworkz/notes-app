707628664510cb0f6c1124cf0c0e3b2a67557ab6
Keyworkz <info@keyworkz.nl>
2026-08-30 03:02:01 +0200

    cleaned added header and pages

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+import NotesPage from "./pages/NotesPage";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <NotesPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="Header">
+      <h1>Header section</h1>
+    </header>
+  );
+}
+
+export default Header;
